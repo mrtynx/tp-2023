dbb1e95aac5a377105a5fef3f71c2f8692195c2f
mrtynx <martin.berki@protonmail.com>
2023-05-28 18:41:05 +0200

    classifier train

--- ml/classifier/model_cnn.py
+++ ml/classifier/model_cnn.py
@@ -4,18 +4,19 @@ from img_dataset import *
 import pathlib
 
 from tensorflow.keras.preprocessing import image as image_utils
+from keras.callbacks import ModelCheckpoint
 
 path = pathlib.Path("sample_img/")
 num_classes = 9
 img_width = 640
 img_height = 640
-batch_size = 32
+batch_size = 16
 
 dataset = tf.keras.preprocessing.image_dataset_from_directory(
     directory=path, image_size=(img_height, img_width), batch_size=batch_size
 ).map(preprocess_images)
 
-val_split = 0.1
+val_split = 0.2
 num_samples = dataset.cardinality().numpy()
 num_val_samples = int(num_samples * val_split)
 num_train_samples = num_samples - num_val_samples
@@ -37,10 +38,15 @@ model = tf.keras.Sequential(
     ]
 )
 
+checkpoints = [
+
+    ModelCheckpoint("../classifier_mdl.h5", save_best_only=True)
+]
+
 model.compile(
     optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
 )
 
 history = model.fit(
-    train_dataset, epochs=20, validation_data=val_dataset, batch_size=batch_size
+    train_dataset, epochs=30, validation_data=val_dataset, batch_size=batch_size, callbacks=checkpoints
 )
